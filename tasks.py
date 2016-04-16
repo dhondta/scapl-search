b1ef0cda5379586d4fb89958b1aead1c2cf0114e
bahmadh <hussein_bahmad@hotmail.com>
2016-04-16 16:06:12 +0200

    problem fix

--- tasks.py
+++ tasks.py
@@ -1,14 +1,7 @@
 from __future__ import absolute_import
 from celery.task import task, Task
 from celery.utils.log import get_task_logger
-from pws import Google, Bing
-import os
 import subprocess
-from celery import Celery
-import time
-import json
-from pprint import pprint
-from argparse import Namespace
 
 logger = get_task_logger(__name__)
 
@@ -23,7 +16,7 @@ class DebugTask(Task):
 def generic(apl_keywords, item_api, item_keywords, item_ns, *args):
     logger.info("Search task...")
     keywords = ",".join([apl_keywords, item_keywords])
-    cmd = 'plugins/' + item_api.format(keywords=keywords, suggestions=item_ns)
+    cmd = 'search/' + item_api.format(keywords=keywords, suggestions=item_ns)
     p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     out, err = p.communicate()
     result = {
@@ -31,11 +24,5 @@ def generic(apl_keywords, item_api, item_keywords, item_ns, *args):
         'error': err,
     }
     
-    return str(out)
+    return result
 
-
-@task( bind=True)
-def error_handler(self, uuid):
-    result = self.app.AsyncResult(uuid)
-    print('Task {0} raised exception: {1!r}\n{2!r}'.format(
-        uuid, result.result, result.traceback))

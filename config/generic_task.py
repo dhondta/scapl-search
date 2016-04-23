740e67662ec379d29fd46573f8bd8972400f30f9
dhondta <alexandre.dhondt@gmail.com>
2016-04-23 15:27:58 +0200

    Refined generic_task config

--- config/generic_task.py
+++ config/generic_task.py
@@ -1,9 +1,13 @@
-GENERIC_TASK_LOG_LEVEL = 2
+import os
+
+
+GENERIC_TASK_LOG_LEVEL = 0
 GENERIC_TASK_CONFIG = {
-    'PROXY_FILE': './proxy_settings',
+#    'PROXY_FILE': os.path.join(os.path.dirname(__file__), 'proxy_settings'),
     'LOG_LEVEL_MAPPING': {
-        0: 30,  # result = main(return_results=True)logging.WARNING
-        1: 20,  # logging.INFO
-        2: 10,  # logging.DEBUG
+        0: 40,  # logging.ERROR
+        1: 30,  # logging.WARNING
+        2: 20,  # logging.INFO
+        3: 10,  # logging.DEBUG
     },
 }

1a1d71f37a1ebf055c01546ad0aa458f328c2232
dhondta <alexandre.dhondt@gmail.com>
2016-04-23 15:25:57 +0200

    Refactord GoogleScraper task runner

--- search/google-scraper/run.py
+++ search/google-scraper/run.py
@@ -1,64 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-__plugin__ = 'GoogleScraper'
-
-import argparse
-import logging
-import os
+import json
 from GoogleScraper import scrape_with_config, GoogleSearchError
-try:
-    import coloredlogs
-    colored_logs_present = True
-except:
-    print("(Install 'coloredlogs' for colored logging)")
-    colored_logs_present = False
+from lib import TaskRunner
+
+
+@TaskRunner.bind(__file__, 'GoogleScraper')
+def run(self):
+    n = self.param['n'] * 10  # multiplier is set to collect far more results than required for establishing suggestions
+    MAX_PER_PAGE = 100        # this is a limit imposed by GoogleScraper
+    config = {
+        'use_own_ip': True,
+        'keyword': self.param['keywords'],
+        'search_engines': ['google'],
+        'scrape_method': 'http',
+        'do_caching': False,
+        'log_level': self.verbose,
+        'num_pages_for_keyword': len(range(0, n, MAX_PER_PAGE)) if n > 0 else 1,
+        'num_results_per_page': min(n, MAX_PER_PAGE) if n > 0 else MAX_PER_PAGE,
+    }
+    if 'PROXY_FILE' in self.config and self.config['PROXY_FILE'] not in [None, '']:
+        config.update({'proxy_file': self.config['PROXY_FILE'], 'check_proxies': False})
+        # NB: check_proxies is a parameter aimed to make the (public) proxy address checked on a website,
+        #     so if using a private network proxy, this check is not required
+    # scrape on keywords and get a connection to the cache database
+    search = scrape_with_config(config)
+    # check the status and raise an exception if scraping failed
+    for serp in search.serps:
+        if serp.status != 'successful' and serp.no_results:
+            self.logger.error(serp.status)
+            exit(1)
+    # collect found links
+    links, suggestions = [link for serp in search.serps for link in serp.links], []
+    k, l = 0, len(links)
+    while len(suggestions) < self.param['n'] and k < l:
+        # TODO: write a filter
+        #  e.g. for:
+        #  - favouring links with domain containing one or more of the keywords)
+        #  - excluding links on specific forums and/or download sites
+        #  - exluding maliious domains acording to Norton Safe Web or other security sources (e.g. Webputation)
+        suggestions.append({'link': links[k].link, 'title': links[k].title, 'text': links[k].snippet})
+        k += 1
+    return suggestions
 
 
 if __name__ == '__main__':
-    os.chdir(os.path.dirname(__file__))
-    # parse input arguments
-    parser = argparse.ArgumentParser(prog=__file__,
-                                     description="Task runner for SCAPL plugin {}.".format(__plugin__),
-                                     epilog="Usage example:\n python3 {} -q test -n 10".format(__file__),
-                                     formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("-c", "--config", dest="config",
-                        help="task configuration dictionary")
-    parser.add_argument("--keywords", help="keywords to be scraped")
-    parser.add_argument("--task", help="task identifier for logging purpose")
-    parser.add_argument("-n", dest="n", type=int, default=0, help="number of results to return [default: 0 (=all)]")
-    parser.add_argument("-v", dest="verbose", action="count", default=0,
-                        help="verbose level [default: 0 (critical)]")
-    args = parser.parse_args()
-    args.config = eval(args.config)
-    # configure logging and get the root logger
-    args.verbose = args.config['LOG_LEVEL_MAPPING'][min(max(args.config['LOG_LEVEL_MAPPING'].keys()), args.verbose)]
-    logging.basicConfig(format='%(name)s - %(asctime)s [%(levelname)s] %(message)s', level=args.verbose)
-    logger = logging.getLogger(args.task)
-    if colored_logs_present:
-        coloredlogs.install(args.verbose)
-    # now, prepare task-specific configuration
-    config = {
-        'use_own_ip': True,
-        'keyword': args.keywords,
-        'search_engines': ['google'],
-        'scrape_method': 'http',
-        'do_caching': True,
-    }
-    if args.n > 0:
-        config.update({'num_pages_for_keyword': args.n})
-    if 'PROXY_FILE' in args.config and args.config['PROXY_FILE'] not in [None, '']:
-        config.update({'proxy_file': args.config['PROXY_FILE'], 'check_proxies': False})
-        # NB: check_proxies is a parameter aimed to make the (public) proxy address checked on a website,
-        #     so if using a private network proxy, this check is not required
-    # try to scrape on keywords and to get a connection to the cache database
-    try:
-        search = scrape_with_config(config)
-    except GoogleSearchError as e:
-        logger.error(str(e))
-        print(e)
-        exit(1)
-    # collect found links
-    links, suggestions = [link for serp in search.serps for link in serp.links], []
-    for link in sorted(links, key=lambda k: k.rank)[:args.n if args.n > 0 else None]:
-        suggestions.append({'link': link.link, 'title': link.title, 'text': link.snippet})
-    print(suggestions)
+    run()
